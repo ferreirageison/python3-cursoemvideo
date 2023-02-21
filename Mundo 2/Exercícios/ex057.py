f4e90bd20784851b5c2b0dfde5a07517ad0ebde0
Geison Ferreira <ferreirainbox@gmail.com>
2023-02-21 20:04:36 +0000

    update exercise solution

--- Mundo 2/Exercícios/ex057.py
+++ Mundo 2/Exercícios/ex057.py
@@ -1,9 +1,10 @@
 # faça um programa que leia o sexo de uma pessoa, mas só aceite os valores 'H'ou 'M'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
 sexo = ''
-sexo = str(input('Qual o seu sexo? [H | M] ')).strip().lower()
-while sexo != 'h' and sexo != 'm':
-    sexo = str(input('Essa opção não é válida. Escolha entre estas duas opções: [H | M]: '))
+sexo = str(input('Qual o seu sexo? [H | M] ')).strip().lower()[0] # [0] pega o primeiro caractere da string
+# while sexo != 'h' and sexo != 'm':
+while sexo not in 'HhMm': #uso de 'not in' diminui e simplifica o código    
+    sexo = str(input('Essa opção não é válida. Escolha entre as duas opções disponíveis: [H | M]: ')).strip().lower()[0]
 if sexo == 'm':    
     print(f'Voce digitou {sexo.upper()} de Mulher.')
 else:
