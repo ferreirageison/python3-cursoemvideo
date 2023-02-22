fd88eae391695642f406aaaad15943922ded1811
Geison Ferreira <ferreirainbox@gmail.com>
2023-02-22 23:14:31 +0000

    update ex resolution

--- Mundo 2/Exercícios/ex060.py
+++ Mundo 2/Exercícios/ex060.py
@@ -1,2 +1,22 @@
 # faça um programa que leia um número qualquer e mostre o seu fatorial usando for e while. ex: 5! = 5*4*3*2*1 = 120
 
+from math import factorial
+
+n = int(input('Digite um número para calcular seu fatorial: '))
+
+print(f'Calculando {n}! = ' , end=' ')
+
+#usando while
+# cont = n
+# while cont > 0:
+#     print(f'{cont}' , end=' ')
+#     print('x' if cont > 1 else '=', end=' ')
+#     cont -= 1
+
+# Usando for    
+for i in range(n, 0, -1):
+    print(f'{i}' , end=' ')
+    print('x' if i > 1 else '=' , end=' ')
+
+print(factorial(n))
+
