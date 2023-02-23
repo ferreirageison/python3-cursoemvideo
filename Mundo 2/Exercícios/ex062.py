da19588c6e9cdbdd3992117a3ed92d2a37e790dc
Geison Ferreira <ferreirainbox@gmail.com>
2023-02-23 23:47:03 +0000

    update exercise solution

--- Mundo 2/Exercícios/ex062.py
+++ Mundo 2/Exercícios/ex062.py
@@ -2,15 +2,18 @@
 
 p = int(input('Digite o primeiro termo: '))
 r = int(input('Digite a razão: '))
-d = p + (10 - 1) * r  # fórmula matemática para encontrar o 10º termo em uma P.A.
-cont = 0
-resp = 's'
-while resp == 's':
-    for i in range(p, d + 1, r):
-        cont += 1
-        print(f'{cont:2}º termo: {i}')
-    resp = str(input('Quer ver os próximos 10 termos? S | N: ')).lower().strip()
-    if resp == 's':
-        p = d + r # atualiza o primeiro termo 'p' com o que seria próximo (11º) da sequencia anterior, usando a razão
-        d = p + (10 - 1) * r # atualiza a formula a partir do primeiro termo atualizado
+
+t = p
+c = 0
+l = 10
+user = 's'
+while user == 's': 
+    print(f'{t}',  end=' ')
+    print('->', end=' ')
+    t += r
+    c += 1
+    if c == l:
+        print('PAUSA')
+        user = str(input('\nDeseja ver os próximos 10 termos? S|N: '))
+        l += 10
 print('Fim')
